d9668af1c40cc9774368768d5779f5afeef3e733
danh@huntlycollege.school.nz <danh@huntlycollege.school.nz>
2024-07-17 06:18:33 +0000

    Created Form1.RowTemplate1 and LICENSE.txt; Edited settings and Form1

--- client_code/Form1/RowTemplate1/__init__.py
+++ client_code/Form1/RowTemplate1/__init__.py
@@ -1,14 +1,14 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import RowTemplate1Template
 from anvil import *
 
 
-class Form1(Form1Template):
+class RowTemplate1(RowTemplate1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
 
-  def Name_pressed_enter(self, **event_args):
+  def text_box_1_pressed_enter(self, **event_args):
     """This method is called when the user presses Enter in this text box"""
     pass
